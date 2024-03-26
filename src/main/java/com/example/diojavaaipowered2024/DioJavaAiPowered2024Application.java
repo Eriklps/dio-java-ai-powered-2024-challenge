61e197568cfd66a24dcd8fad00c9c92c3c7dcf4a
Eriklps <erikdslopes@gmail.com>
2024-03-26 15:49:06 -0300

    Initial commit

---
+++ src/main/java/com/example/diojavaaipowered2024/DioJavaAiPowered2024Application.java
@@ -0,0 +1,13 @@
+package com.example.diojavaaipowered2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioJavaAiPowered2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioJavaAiPowered2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/diojavaaipowered2024/DioJavaAiPowered2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diojavaaipowered2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioJavaAiPowered2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
